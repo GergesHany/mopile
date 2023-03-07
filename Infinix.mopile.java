d6216ac79683350accaf1f94547fae38f1d8bced
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-03-07 21:40:44 +0200

    Update and rename Infinix.mopile to Infinix.mopile.java

---
+++ Infinix.mopile.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Infinix extends basic_of_mopile{
+
+	public Infinix() {
+		super(6, "Infinix", "Note 8", "Octa-core", "128 GB", "6 GB", "64 MP", "16 MP", "9.95 inches", "5200 mAh", "160 $", "");
+	}
+
+}
